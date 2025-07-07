dbd89d4b618bac468646174e0162c9afd7c4eb69
Anuragroyan <7619354965anurag@gmail.com>
2025-07-07 20:16:35 +0530

    my first initial commit for this app

---
+++ CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by Dungeon_master on 24/05/25.
+//
+
+import Testing
+@testable import CalculatorApp
+
+struct CalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
